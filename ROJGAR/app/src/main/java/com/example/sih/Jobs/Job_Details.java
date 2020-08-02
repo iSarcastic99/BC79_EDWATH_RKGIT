96608f79d33937a672f4f6080f6261df11db4f8f
Hardik17 <17hardik.sharma@gmail.com>
2020-08-02 09:38:26 +0530

    Publish vocational job added

--- ROJGAR/app/src/main/java/com/example/sih/Jobs/Job_Details.java
+++ ROJGAR/app/src/main/java/com/example/sih/Jobs/Job_Details.java
@@ -35,15 +35,14 @@ import java.io.InputStream;
 public class Job_Details extends AppCompatActivity {
 
     private TextView job_post, company_name, company_location, job_details, salaryLabel, salary, sectorLabel, sector, jobDescriptionLabel, jobDescription;
-    DatabaseReference reff, reff1;
-    int k, i, x, y, d, size;
+    DatabaseReference reff;
+    int k, i, x;
     String M, J, C, check, phone, activity, S, jobReference, jobCategory, domainType, post, name, location, Salary, Sector, jobDesc, TAG;
     Translate translate;
     Firebase firebase;
-    Button FavButton, roadmap, apply;
+    Button apply;
     Intent intent;
     ProgressDialog pd;
-    Boolean isStored = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +65,6 @@ public class Job_Details extends AppCompatActivity {
         domainType = intent.getStringExtra("domainType");
         TAG = intent.getStringExtra("tag");
         job_post = findViewById(R.id.job_post);
-        FavButton = findViewById(R.id.favButton);
-        roadmap = findViewById(R.id.roadmapButton);
         apply = findViewById(R.id.apply);
         company_name = findViewById(R.id.company_name);
         company_location = findViewById(R.id.company_location);
@@ -84,46 +81,6 @@ public class Job_Details extends AppCompatActivity {
         pd = new ProgressDialog(Job_Details.this);
         pd.setMessage("Getting Job Details");
         pd.show();
-        if(!check.equals(getResources().getString(R.string.english))){
-            FavButton.setText("ड्रीमजॉब में जोड़ें");
-        }
-
-        FavButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try {
-                    reff1 = FirebaseDatabase.getInstance().getReference().child("Users").child(phone).child("Dream Jobs");
-                    reff1.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            long childCount = snapshot.getChildrenCount();
-                                firebase.child(phone).child("Dream Jobs").child(domainType + " " + TAG + " " + jobCategory + " "+ jobReference).setValue(domainType + "-" + TAG + "-" + jobCategory + "-" + jobReference);
-                                isStored = true;
-                                if(check.equals(getResources().getString(R.string.english))) {
-                                    Toast.makeText(Job_Details.this, "Saved to Dream Jobs", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(Job_Details.this, "ड्रीमजॉब में जोड़ा गया", Toast.LENGTH_SHORT).show();
-                                }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-                } catch (Exception e) {
-
-                    firebase.child(phone).child("Dream Jobs").child(domainType + " " + TAG + " " + jobCategory + " "+ jobReference).setValue(domainType + "-" + TAG + "-" + jobCategory + "-" + jobReference);
-                    isStored = true;
-                    if(check.equals(getResources().getString(R.string.english))) {
-                        Toast.makeText(Job_Details.this, "Saved to Dream Jobs", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(Job_Details.this, "ड्रीमजॉब में जोड़ा गया", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
 
         if (!check.equals(getResources().getString(R.string.english))){
             getTranslateService();
@@ -169,18 +126,6 @@ public class Job_Details extends AppCompatActivity {
 
             }
         });
-
-        if (activity.equals("Main")){
-
-            roadmap.setVisibility(View.VISIBLE);
-
-        }
-
-        else{
-
-            FavButton.setVisibility(View.VISIBLE);
-
-        }
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
